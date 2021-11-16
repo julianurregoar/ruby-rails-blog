5c6cb1d6e46b12f6b9ce2d25e2e16ff742149a1c
Julian Urrego <julian.urrego.ar@gmail.com>
2021-11-16 16:18:26 -0500

    feat: Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+    # all
+    def index
+        @articles = Article.all
+    end
+
+    # find by id
     def show
         @article = Article.find(params[:id])
     end
@@ -6,7 +12,7 @@ class ArticlesController < ApplicationController
     def new    
     end
 
-   
+    # create
     def create
         @article = Article.new(article_params)
     
@@ -14,6 +20,7 @@ class ArticlesController < ApplicationController
         redirect_to @article
     end
     
+    # private method to accept a POST request (create and update)
     private
         def article_params
         params.require(:article).permit(:title, :text)
