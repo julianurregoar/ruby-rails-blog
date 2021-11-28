9632f81f17639e7e75a6f9d57d7595c16988b0a5
Julian Urrego <julian.urrego.ar@gmail.com>
2021-11-28 12:00:52 -0500

    feat: Deleting Articles and add prettier formatter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,20 +8,19 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
-    
+
     def new
         @article = Article.new
     end
-    
+
     # edit
     def edit
         @article = Article.find(params[:id])
-      end
+    end
 
     # create
     def create
         @article = Article.new(article_params)
-    
 
         if @article.save
             redirect_to @article
@@ -29,10 +28,10 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-    
+
     def update
         @article = Article.find(params[:id])
-        
+
         if @article.update(article_params)
             redirect_to @article
         else
@@ -40,9 +39,18 @@ class ArticlesController < ApplicationController
         end
     end
 
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    
+        redirect_to articles_path
+    end
+
     # private method to accept a POST request (create and update)
     private
-        def article_params
+
+    def article_params
         params.require(:article).permit(:title, :text)
-        end
+    end
 end
